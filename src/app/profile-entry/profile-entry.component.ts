9d390b6e2978d9b69a00ab9b7e6760d4f241cb14
meganla <meganlarson415@gmail.com>
2018-02-10 15:16:01 -0500

    Merge branch 'master' of https://github.com/tylercote/spot-a-match into profile-entry
    # Conflicts:
    #	src/app/app.module.ts

--- src/app/profile-entry/profile-entry.component.ts
+++ src/app/profile-entry/profile-entry.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile-entry.component.html',
   styleUrls: ['./profile-entry.component.css']
 })
 export class ProfileEntryComponent implements OnInit {
-
+  idOne = '';
+  idTwo = '';
   constructor() { }
 
   ngOnInit() {
