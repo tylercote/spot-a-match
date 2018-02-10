aacaa3d9d7306d1a15e94883cfba97399b82c318
Tyler Cote <cote.t@husky.neu.edu>
2018-02-09 21:40:43 -0500

    Added ID entry ocmponent and started services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Profile } from './profile.entry/profile.entry.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Profile.EntryComponent
   ],
   imports: [
     BrowserModule
