4c50248d54df21aa063f669ddee3c6146b24be04
Daniel Gao <danielgaooooo@gmail.com>
2018-02-10 17:12:54 -0500

    updated website

--- src/app/comparison/comparison.component.ts
+++ src/app/comparison/comparison.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComparisonComponent implements OnInit {
 
+  user1Danciness;
+  user2Danciness;
   constructor() { }
 
   ngOnInit() {
