373d45421b867ed4c321894456801e7f4234054a
meganla <meganlarson415@gmail.com>
2018-02-10 10:06:19 -0500

    bootstrap stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-import { Profile } from './profile.entry/profile.entry.component';
+import { Profile } from './profile-entry/profile-entry.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Profile.EntryComponent
+    Profile
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
