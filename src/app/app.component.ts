b543efec731481dd3209713c322ec2e349ec38d2
Tyler Cote <adcbl8x@adcbl8x-mbp.local>
2018-02-10 22:09:44 -0500

    data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import * as Spotify from 'spotify-web-api-js';
 import PlaylistTrackObject = SpotifyApi.PlaylistTrackObject;
 import * as $ from 'jquery';
+import {isNumber} from 'util';
 
 const s = new Spotify();
 
@@ -13,67 +14,176 @@ const s = new Spotify();
 
 export class AppComponent implements OnInit {
   title = 'app';
-  user1ID;
-  user2ID;
-  user1Tracks: PlaylistTrackObject[] = [];
-  user2Tracks: PlaylistTrackObject[] = [];
-  user1Genres = {};
-  user2Genres = {};
+  user1;
+  user2;
+  @Input()
+  data;
 
-  constructor() {}
+  static possibleGenres = {
+    'classical': 0,
+    'hip hop': 0,
+    'jazz blues': 0,
+    'country': 0,
+    'edm': 0,
+    'rock': 0,
+    'indie pop': 0,
+    'pop': 0
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  getDataFromChild($event) {
+
+  async getDataFromChild($event) {
     s.setAccessToken(this.getAccessToken());
-    this.user1ID = $event[0];
-    this.user2ID = $event[1];
-    const self = this;
-    s.getUserPlaylists(this.user1ID).then( data => data.items.map(
-      item => s.getPlaylistTracks(this.user1ID, item.id).then(result => {
-        result.items.forEach(track => {
-          self.user1Tracks.push(track);
-
-          this.getGenres(track).then(genres => genres.forEach((genre) => {
-            let x = this.addGenre(self.user1Genres, genre);
-            console.log(x)
-            self.user1Genres = x
-          }));
-        });
-      })
-    )).then(blah => console.log(self.user1Genres));
-
-    s.getUserPlaylists(this.user2ID).then(data => data.items.map(
-      item => s.getPlaylistTracks(this.user2ID, item.id).then(result => {
-        result.items.forEach(
-          track => self.user2Tracks.push(track)
-        );
-      })
-    ));
-    console.log(this.user1Tracks);
-    console.log(this.user2Tracks);
+    let user1ID = $event[0];
+    this.user1 = await this.buildUser(user1ID);
+    let user2ID = $event[1];
+    this.user2 = await this.buildUser(user2ID);
+    this.data = this.makeData(this.user1, this.user2);
+    console.log(this.data)
   }
+
+  makeData(user1, user2) {
+    let u1g = user1['genres'];
+    let u2g = user2['genres'];
+
+    return {
+      "jazz":       [u1g['jazz blues'],       u2g['jazz blues'], 1],
+      "classical":  [u1g['classical'],  u2g['classical'], 1],
+      'hiphop':     [u1g['hip hop'],    u2g['hip hop'], 1],
+      'bluesjazz':  [u1g['jazz blues'], u2g['jazz blues'], 1],
+      'country':    [u1g['country'],    u2g['country'], 1],
+      'edm':        [u1g['edm'],        u2g['edm'], 1],
+      'rock':       [u1g['rock'],       u2g['rock'], 1],
+      'indie':      [u1g['indie pop'],  u2g['indie pop'], 1],
+      'pop':        [u1g['pop'],        u2g['pop'], 1],
+      'musicpopularity': [user1['popularity'], user2['popularity'], 2],
+      'instrumentalness': [15, 39, 2]
+    }
+  }
+
+  async sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async buildUser(userId) {
+
+    const response = await this.getUserTracks(userId);
+    const tracks = response['tracks'];
+    const popularity = response['popularity'];
+    await this.sleep(2000);
+    console.log(tracks);
+    const artists = await this.getUserArtists(tracks);
+    await this.sleep(6000);
+    console.log(artists);
+    const genres = await this.getUserGenres(artists);
+    await this.sleep(2000);
+    console.log(genres);
+
+    const user = {
+      'id': userId,
+      'tracks': tracks,
+      'artists': artists,
+      'genres': genres,
+      'popularity': Math.round(popularity[0] / tracks.length),
+
+    };
+    console.log(user);
+    return user;
+
+  }
+
+  async getUserTracks(id) {
+    const tracks = [];
+    let popularity = [0];
+
+    return await s.getUserPlaylists(id).then(data => {
+      return data.items.forEach(async item => {
+        return await s.getPlaylistTracks(id, item.id).then(result => {
+          return result.items.forEach(track => {
+            if (!isNaN(track.track.popularity)) {
+              popularity[0] += track.track.popularity;
+            }
+            return tracks.push(track);
+          });
+        });
+      });
+    }).then(data => {
+      return {
+        'tracks': tracks,
+        'popularity': popularity
+      };
+    });
+  }
+
+  async getUserArtists(tracks) {
+    let artists = [];
+    await tracks.forEach(async track => {
+      let fullHref = track.track.artists[0].href;
+
+      if (fullHref === null) {
+        return [];
+      }
+
+      let realHref = fullHref.match(/(.*?)artists\/(.*)/)[2];
+      let artist = await s.getArtist(realHref);
+      artists.push(artist);
+    });
+    return artists;
+  }
+
+  async getUserGenres(artists) {
+    const genres = JSON.parse(JSON.stringify(AppComponent.possibleGenres));
+    artists.forEach(artist =>
+      artist.genres.forEach(genre =>
+        this.addGenre(genres, genre)));
+    return genres;
+  }
+
+  /*
+    getDataFromChild2($event) {
+      s.setAccessToken(this.getAccessToken());
+      let user1ID = $event[0];
+      this.buildUser(user1ID);
+
+      let user2ID = $event[1];
+      this.buildUser(user2ID);
+
+      const self = this;
+      s.getUserPlaylists(this.user1ID).then( data => data.items.map(
+        item => s.getPlaylistTracks(this.user1ID, item.id).then(result => {
+          result.items.forEach(track => {
+            self.user1Tracks.push(track);
+
+            this.getGenres(track).then(genres => genres.forEach((genre) => {
+              let x = this.addGenre(self.user1Genres, genre);
+              console.log(x)
+              self.user1Genres = x
+            }));
+          });
+        })
+      ));
+
+      s.getUserPlaylists(this.user2ID).then(data => data.items.map(
+        item => s.getPlaylistTracks(this.user2ID, item.id).then(result => {
+          result.items.forEach(
+            track => self.user2Tracks.push(track)
+          );
+        })
+      ));
+      console.log(this.user1Tracks);
+      console.log(this.user2Tracks);
+    }*/
 
   getAccessToken() {
     const hash = window.location.hash;
     return hash.match(/(.*?)=(.*?)&/)[2];
   }
 
-  /*
-  Checks for compatibility of genres.
-   */
-  async getGenreCompatibility(tracks, genres) {
-    tracks.forEach(track => {
-        $.getJSON(track.track.album.href).then(album => {
-          console.log(album)
-          album.genres.forEach(genre => {
-            genres = this.addGenre(genres, genre);
-          });
-        });
-      }
-    );
-  }
 
   async getGenres(track) {
     let fullHref = track.track.artists[0].href;
@@ -84,17 +194,15 @@ export class AppComponent implements OnInit {
 
     let realHref = fullHref.match(/(.*?)artists\/(.*)/)[2];
     let x = s.getArtist(realHref).then(artist => artist.genres);
-    return x
+    return x;
   }
-
 
   addGenre(genreMap, newGenre) {
     if (newGenre in genreMap) {
       genreMap[newGenre] = genreMap[newGenre] + 1;
     } else {
-      genreMap[newGenre] = 1;
+      // genreMap[newGenre] = 1;
     }
-    console.log(genreMap);
     return genreMap;
   }
 }
